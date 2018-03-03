8131ca41d9cae6bd4b30d11ad0a18bc506bf23a1
Kara Cavanaugh <karacav@msn.com>
2018-03-03 15:07:38 -0500

    added API

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const apiKey = 'ce8c46a0fb4e443b92a3f4e2fe584949';
+
+const main = document.querySelector('main');
 window.addEventListener('load', e => {
   updateNews();
   console.log('works');
 });
-async function updateNews(params) {
+async function updateNews() {
   const response = await fetch(
-    `https://newsapi.org/v2/sources?apiKey=${apiKey}`
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=ce8c46a0fb4e443b92a3f4e2fe584949`
   );
-  const json = await response.json;
+
+  const json = await response.json();
+
+  main.innerHTML = json.articles.map(createArticle).join('\n');
+
+  function createArticle(article) {
+    return `
+    <div class="article">
+<h1>${article.title}</h1>
+<div class="brief">
+<p>${article.description}</p>
+</div>
+    </div>
+    `;
+  }
 }
